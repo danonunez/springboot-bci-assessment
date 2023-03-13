e25d841b18545798cbbef738f6c5fcdcc52f49bc
Dano Núñez <danonunez393@gmail.com>
2023-03-13 16:42:13 -0300

    Add springboot basics

---
+++ src/main/java/cl/danonunez/springbootbciassessment/SpringbootBciAssessmentApplication.java
@@ -0,0 +1,13 @@
+package cl.danonunez.springbootbciassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBciAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBciAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/danonunez/springbootbciassessment/SpringbootBciAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.danonunez.springbootbciassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBciAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
